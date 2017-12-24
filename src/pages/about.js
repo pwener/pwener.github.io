c0223ece05d0a8b2ea92667c5468a77fbb92705b
pwener <phwener@gmail.com>
2017-12-24 19:33:52 -0200

    Fixing about

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,20 +22,20 @@ class AboutPage extends React.Component {
                 <h2>Phelipe Wener</h2>
                 <div className="content">
                     <p>
-                        Lembro de uns 10 anos atrás, quando modificava o html de alguns blogs, tentando criar o visual ideal.
-                        Essa vontade de criar, junto ao fascinio do impacto das redes sociais que ainda ganhavam força, me 
-                        fez naquela mesma época entrar em um curso técnico de HTML/CSS. Não demorou para perceber que 
-                        havia muito a se criar, o mundo estava em um grande processo de informatização e eu estava disposto a 
-                        colaborar nisso. Não satisfeito com as habilidades conquistadas no meu curso técnico de <i>webdesign</i>, 
+                        Lembro que a 10 anos atrás, quando mantia alguns blogs, tentando sempre criar o visual ideal.
+                        Essa vontade, junto ao fascinio do impacto das redes sociais, que ainda ganhavam força, me 
+                        fizeram naquela época entrar em um curso técnico de HTML/CSS. Não demorou para perceber que 
+                        havia muito a se criar, o mundo estava em um grande processo de informatização e eu estava ansioso para 
+                        colaborar com isso. Não satisfeito com as habilidades conquistadas no meu curso técnico de <i>webdesign</i>, 
                         decidi fazer um curso técnico de Java, que me ajudou a encarar muitas dificuldades futuras, foi ainda a
-                        mais uma motivação para cursar Engenharia de Software.
+                        maior motivação para cursar Engenharia de Software.
                     </p>
                     <p>
                         Apenas na universidade, consegui compreender o que muitos da área não notam: Software é um 
                         produto extremamente complexo, difícil de se criar, mais difícil ainda de se manter, o que exige muita
-                        competência e reflexão a cada passo do seu ciclo de vida. Desse quadro, fiquei interessado em várias áreas 
-                        da Engenharia de Software que são cruciais para prover qualidade, esses serão os assuntos  chave o qual 
-                        pretendo discursar nesse blog.
+                        competência e uma constante reflexão a cada passo do seu ciclo de vida. Desse quadro, fiquei interessado 
+                        em vários tópicos da Engenharia de Software, se tornou um vício descobrir como prover qualidade ao software
+                        e seu processo de criação, basicamente esse é o tema chave o qual pretendo discursar nesse blog.
                     </p>
                 </div>
             </div>
