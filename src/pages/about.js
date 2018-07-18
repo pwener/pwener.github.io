e4ed466a1cb0434b42b9a09b81b2e8ff2295e5ad
Phelipe Wener <phwener@gmail.com>
2018-07-18 11:03:20 -0300

    Fixing about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import profilePic from '../assets/profile-pic.jpg'
+import aboutPic from '../assets/about-pic.jpg'
 import { rhythm } from '../utils/typography'
 
 import '../components/hardStyles.css'
@@ -10,40 +10,46 @@ class AboutPage extends React.Component {
         return (
             <div className="about">
                 <img
-                    src={profilePic}
+                    src={aboutPic}
                     alt={`Phelipe Wener`}
                     style={{
                         marginRight: rhythm(1 / 2),
                         marginBottom: 0,
                         width: rhythm(7),
-                        height: rhythm(7),
+                        borderRadius: '50%'
                     }}
                 />
-                <h2 style={{marginBottom: rhythm(2)}}>Phelipe Wener</h2>
+                <h2 style={{marginBottom: rhythm(2)}}>Sobre mim</h2>
                 <div className="content">
                     <p>
-                      Sou sujeito que gosta de muitas coisas, sempre aberto a tentar
-                      e aprender. Adoro conversas estranhas, principalmente se for
-                      pra criar teorias malucas de filmes ou séries.
+                      Sempre aberto a tentar e aprender. Sou sujeito que gosta
+                      de muitas coisas e odeia outras na mesma
+                      intensidade(risos).
+                      Adoro conversas estranhas, principalmente se for pra criar
+                      teorias malucas de filmes ou séries. Gosto também de
+                      música, um bom café, viajar, praticar esportes e um
+                      barzinho com os amigos.
                     </p>
                     <p>
-                      Por volta de uns 10 anos atrás, lembro que um dos meus hobbies era blogar.
-                      Passava horas criando o visual de um ou outro blog que nem mesmo chegava a publicar.
-                      Naquele momento me dedicava a uma busca pelo visual ideal. Essa vontade, junto ao
-                      fascínio do impacto das redes sociais, que ainda ganhavam força, me fizeram naquela
-                      época entrar em um curso técnico de HTML/CSS. Não demorei a perceber que havia muito
-                      a se criar, o mundo estava em um grande processo de informatização e eu estava ansioso para colaborar com isso.
-                      Aquele curso técnico tinha me mostrado a ponta do iceberg, havia muito a aprender!
-                      Decidi então fazer um curso técnico de Java, que me ajudou a encarar muitas dificuldades futuras,
-                      foi ainda a maior motivação para cursar Engenharia de Software.
+                      Profissionalmente, tudo começou por volta de uns 10 anos
+                      atrás. Passava horas criando o visual de um ou outro blog.
+                      Essa vontade, junto ao fascínio do impacto das redes
+                      sociais, que ainda ganhavam força, me fizeram naquela
+                      época entrar em um curso técnico de HTML/CSS. Não demorei
+                      a perceber que havia muito a se criar, o mundo estava em
+                      um grande processo de informatização e eu estava ansioso
+                      para colaborar com isso. Aquele curso técnico tinha me
+                      mostrado a ponta do iceberg, havia muito a aprender!
+                      Decidi então fazer um curso técnico de Java, que me ajudou
+                      a encarar muitas dificuldades futuras, foi a maior
+                      motivação para cursar Engenharia de Software.
                     </p>
                     <p>
-                      Apenas na universidade, consegui compreender o que muitos da área não percebem:
-                      Software é um produto extremamente complexo, difícil de se criar, mais difícil ainda de se manter,
-                      o que exige muita competência e uma constante reflexão a cada passo do seu ciclo de vida.
-                      Dessa forma fiquei interessado em vários tópicos da Engenharia de Software, se tornou um
-                      vício descobrir como prover qualidade ao software e ao seu processo de criação, basicamente
-                      esse é o tema chave o qual pretendo discursar nesse blog.
+                      Na universidade, me tornei interessado em vários tópicos
+                      da Engenharia de Software, se tornou um vício descobrir
+                      como prover qualidade ao software e seu processo de
+                      criação, basicamente esse é o tema chave o qual pretendo
+                      discursar nesse blog.
                     </p>
                 </div>
             </div>
